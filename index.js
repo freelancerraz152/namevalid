0fc3f80fc96fcb095860a403e9e9519bf7b896e2
MD RAZ <raz46418316@gmail.com>
2022-04-17 04:06:41 +0600

    get result dynamicaly

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-// fullname
-let fullName = prompt('write your Full Name',"md raz");
-let words = fullName.split(' ');
-words = words.map(function (value) {
-	return value.charAt(0).toUpperCase() + value.slice(1);
-});
-console.log(words);
+function getResult(mark) {
+	if (mark > 100) return `your mark not a valid`;
+	else if (mark >= 80) return `your mark is ${mark} & Grade is A+`;
+	else if (mark >= 70) return `your mark is ${mark} & Grade is A`;
+	else if (mark >= 60) return `your mark is ${mark} & Grade is A-`;
+	else if (mark >= 50) return `your mark is ${mark} & Grade is B`;
+	else if (mark >= 40) return `your mark is ${mark} & Grade is C`;
+	else if (mark >= 33) return `your mark is ${mark} & Grade is D`;
+	else if (mark < 33) return `your mark is ${mark} & Grade is F`;
+	else return `your mark not a valid`;
+}
+let getMark = prompt('Enter your mark', 90);
 
-fullName = words.join(' ');
-
-document.getElementById('demo').innerHTML = fullName;
-document.getElementById('demo').style.color = '#9acd32';
+document.getElementById('demo').innerHTML = getResult(getMark);
+document.getElementById('demo').style.color = 'tomato';
+document.getElementById('demo').style.fontSize = '20px';
